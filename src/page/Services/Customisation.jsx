caf087b852586fdc8aa981f32da0ee2f76a1ceba
STharshan <sivatharshan81@gmail.com>
2026-02-22 19:20:28 +0530

    add service

--- src/page/Services/Customisation.jsx
+++ src/page/Services/Customisation.jsx
@@ -10,8 +10,7 @@ const Customisation = () => {
         <div>
             <HeroSection />
             <AboutSection />
-            <ProblemSection />
-          
+            <ProblemSection /> 
             <FAQSection />
         </div>
     )
